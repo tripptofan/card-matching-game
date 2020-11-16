5cce3e3eec5e31c3cfd4c201c5f3201ecec5fa38
unknown <tripptofan@gmail.com>
2020-11-16 13:34:24 -0500

    Added new deployment link in readme

--- src/App.js
+++ src/App.js
@@ -3,36 +3,13 @@ import './App.css';
 import Fade from 'react-reveal/Fade';
 
 
-/*Images*/
-// import pic0 from './Images/0.jpg' 
-// import pic1 from './Images/1.jpg' 
-// import pic2 from './Images/2.jpg' 
-// import pic3 from './Images/3.jpg' 
-// import pic4 from './Images/4.jpg' 
-// import pic5 from './Images/5.jpg' 
-// import pic6 from './Images/6.jpg' 
-// import pic7 from './Images/7.jpg' 
-// import pic8 from './Images/8.jpg' 
-// import pic9 from './Images/9.jpg' 
-// import pic10 from './Images/10.jpg' 
-// import pic11 from './Images/11.jpg' 
-// import pic12 from './Images/12.jpg' 
-// import pic13 from './Images/13.jpg' 
-// import pic14 from './Images/14.jpg' 
-// import pic15 from './Images/15.jpg' 
-// import pic16 from './Images/16.jpg' 
-// import pic17 from './Images/17.jpg' 
-
-
-
 function App() {
 
-const [timer, setTimer] = useState();
+
 const [score, setScore] = useState(0);
 const [cards, setCards] = useState();
 const [flipped, setFlipped] = useState([])
 
-// const images = [pic0,pic1,pic2,pic3,pic4,pic5,pic6,pic7,pic8,pic9,pic10,pic11,pic12,pic13,pic14,pic15,pic16,pic17,pic0,pic1,pic2,pic3,pic4,pic5,pic6,pic7,pic8,pic9,pic10,pic11,pic12,pic13,pic14,pic15,pic16,pic17];
 
 const context = require.context("./Images", true, /\.(jpg)$/);
 const images = context.keys().map(img => context(img)).reduce((res, current) => {
@@ -44,10 +21,8 @@ function shuffle(array) {
   var currentIndex = array.length, temporaryValue, randomIndex;
 
   while (0 !== currentIndex) {
-
     randomIndex = Math.floor(Math.random() * currentIndex);
     currentIndex -= 1;
-
     temporaryValue = array[currentIndex];
     array[currentIndex] = array[randomIndex];
     array[randomIndex] = temporaryValue;
@@ -56,14 +31,12 @@ function shuffle(array) {
 }
 
 const initialize = () => {
-  
   if(!cards){
     document.getElementById('score').classList.toggle('active');
   }
   let arr = shuffle(images);
   setCards(arr.map((i,index) => ({card: i, id: index})));
   setScore(0);
-  
 }
 
 const flip = card => {
